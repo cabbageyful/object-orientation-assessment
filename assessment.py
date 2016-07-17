79b819b85d4bc7633d36f2282504e02ad6751f22
Jen Dixon <jendixon85@gmail.com>
2016-07-17 19:53:26 +0000

    Created abstract student & abstract question objects w/ dictionaries containing the label and appropriate info for each.

--- assessment.py
+++ assessment.py
@@ -33,14 +33,66 @@ Part 1: Discussion
 
 
 """
+# Parts 2 through 5:
+# Create your classes and class methods
+
 
 class AbstractStudent(object):
-    """Template for student info. """
+    """Returns student first & last name, address in a dictionary. """
 
     def __init__(self, first_name, last_name, address):
-        
+        """Creates new student as an object in the program."""
+
         self.first_name = first_name
         self.last_name = last_name
         self.address = address
-# Parts 2 through 5:
-# Create your classes and class methods
+
+        # Creates dictionary for each student with the label & info.
+
+        self.info = {
+            'first name': self.first_name,
+            'last name': self.last_name,
+            'address': self.address,
+        }
+
+
+jasmine = AbstractStudent('Jacqui', 'Console', '888 Binary Ave')
+
+
+class AbstractQuestion(object):
+    """Template for adding separate questions as their own object w/ answer. """
+
+    def __init__(self, question, answer):
+        """Adds the question & answer attributes to each new object."""
+
+        self.question = question
+        self.answer = answer
+
+        # Will show the question & correct answer in a dictionary.
+
+        self.correct_answer = {
+            'question': self.question,
+            'correct_answer': self.answer
+        }
+
+
+trick = AbstractQuestion('What is your favorite color?', 'Nobody cares.')
+
+
+class AbstractExam(object):
+    """Template for creating an exam. """
+
+    def __init__(self, exam_name):
+
+        self.exam_name = exam_name
+        self.questions = []   # Empty list to add question objects.
+
+        self.grading_guide
+
+
+        
+
+
+
+
+
